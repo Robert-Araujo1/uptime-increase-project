7b674e652b8adcecf69ddaeabdd0f3165316c2ee
Robert <robert.araujo15@gmail.com>
2024-02-03 22:10:06 -0300

    feat: :sparkles: added validation icon on map

--- src/components/UIPMap/UIPMarker.js
+++ src/components/UIPMap/UIPMarker.js
@@ -4,17 +4,24 @@ import motorGrader from '../../assets/images/machines/motor-grader.svg';
 import loader from '../../assets/images/machines/loader.svg';
 import backhoe from '../../assets/images/machines/backhoe.svg';
 import crawlerDozer from '../../assets/images/machines/crawler-dozer.svg';
+import attentiton from '../../assets/images/validation/attentiton.svg';
+import completed from '../../assets/images/validation/completed.svg';
+import inProgress from '../../assets/images/validation/inProgress.svg';
+
 import { getRandomByValues } from '../../assets/data/utils/dataManipulation';
 
 const models = [excavator, motorGrader, loader, backhoe, crawlerDozer];
+const validations = [completed, attentiton, inProgress];
 
 export default () =>
   L.icon({
     iconUrl: getRandomByValues(models),
+    shadowUrl: getRandomByValues(validations),
+
     iconSize: [32, 32],
-    shadowSize: [50, 64],
+    shadowSize: [24, 24],
     iconAnchor: [12, 31],
-    shadowAnchor: [4, 62],
+    shadowAnchor: [23, 43],
     popupAnchor: [-3, -76],
     className: 'uip-marker',
   });
