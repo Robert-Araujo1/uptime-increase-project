a538983a45e3f862c6e3639fd24004d5c8ba30ed
Robert Araújo <robert.araujo15@gmail.com>
2024-09-19 15:16:38 -0300

    refactor: filtering ws message

--- src/components/UIPMap/index.jsx
+++ src/components/UIPMap/index.jsx
@@ -37,8 +37,10 @@ export default () => {
     fetchVehicles();
 
     const socket = new WebSocket(WSSERVER);
-    socket.onmessage = async (event) => {
-      await fetchVehicles();
+    socket.onmessage = async ({ data }) => {
+      if (data === 'vehicles.table.updated!') {
+        await fetchVehicles();
+      }
     };
   }, []);
 
