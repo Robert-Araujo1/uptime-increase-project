b2139a5051b4efa13b317a531aaa622ac2cf4536
Robert <robert.araujo15@gmail.com>
2025-01-29 15:36:10 -0300

    feat: adding specials customers power bi option

--- src/components/UIPHomeOffcanvas/OffcanvasMenuList.jsx
+++ src/components/UIPHomeOffcanvas/OffcanvasMenuList.jsx
@@ -2,6 +2,7 @@ import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import BuildIcon from '@mui/icons-material/Build';
 import { ReactComponent as POPSIcon } from '../../assets/icons/POPSIcon.svg';
+import PowerBIIcon from '../../assets/icons/pbi.svg';
 import i18next from '../../i18n/i18n';
 import { useLocation } from 'react-router-dom';
 
@@ -29,6 +30,11 @@ export default () => {
       name: 'POPS Connect',
       icon: <POPSIcon fill='currentColor' style={{ width: 30, height: 30 }} />,
     },
+    {
+      href: 'https://app.powerbi.com/links/V2phNgENyb?ctid=76bddcdc-73b4-44d6-ba79-bb3f91ed8101&pbi_source=linkShare',
+      name: 'BI Clientes Estratégicos',
+      icon: <img width={15} height={15} src={PowerBIIcon} alt='Power BI' />,
+    },
   ];
   return (
     <div className='menu-list d-flex align-items-start flex-column mb-3 w-100'>
@@ -37,11 +43,13 @@ export default () => {
         id='menu'>
         {items.map((item, index) => {
           return (
-            <NavItem
-              key={index}
-              {...item}
-              isActive={location.pathname == item.href ? 'active' : ''}
-            />
+            <>
+              <NavItem
+                key={index}
+                {...item}
+                isActive={location.pathname == item.href ? 'active' : ''}
+              />
+            </>
           );
         })}
       </ul>
@@ -52,7 +60,10 @@ export default () => {
 const NavItem = ({ href, name, icon, isActive }) => {
   return (
     <li className='nav-item w-100 mb-4'>
-      <a href={href} className={`nav-link ${isActive} align-middle`}>
+      <a
+        href={href}
+        className={`nav-link ${isActive} align-middle`}
+        target={name === 'BI Clientes Estratégicos' ? '_blank' : '_self'}>
         {icon}
         <span className='ms-1 d-sm-inline px-2'>{name}</span>
       </a>
